c708aabd3b7b86c9b877163dc361c5e8b62f2488
Mishkail <pankovmo@gmail.com>
2019-02-06 18:33:27 +0300

    add solution of inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
+## add matrix into the cash
 makeCacheMatrix <- function(x = matrix()) {
-
+    if (dim(x)[1] != dim(x)[2]) {
+        message('Warrning, initial matrix should be square for inversing')
+    } 
+    m <- NULL
+    setm <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    getm <- function() x # our initial data
+    setsolve <- function(solve) m <<- solve # function of inverse
+    getsolve <- function() m # function of inverse of our initial data
+    list(setm = setm, getm = getm, 
+         setsolve = setsolve,
+         getsolve = getsolve)
+    
 }
 
 
-## Write a short comment describing this function
+## solve inverse of square matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    # if it has already solved, then return value from cache
+    if (!is.null(m)) {
+        message("getting cached data") 
+        return(m)
+    } # if m is null then we solve inversion of new matrix
+    data <- x$getm()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
